edf15927d17791642e325bdb9f5302a6c537f2a6
Mustafa Kemal Mayuk <mkemalmayuk@Mustafas-MacBook-Pro.local>
2019-08-16 16:22:04 +0300

    first commit

---
+++ hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	print("my app!\n")
+	fmt.Println("hello")
+}
---
+++ http_server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This is a website server by a Go HTTP server.")
+	})
+
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if r.Method == "GET" {
+			fmt.Fprintf(w, "Pong")
+		}
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":3001", nil)
+}
